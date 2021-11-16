a4a7f36a68ecde3580467990cb52a0456071ecd0
S.a <shanenprz@gmail.com>
2021-11-16 16:21:46 +0000

    changed repo name

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
   <body>
     <h1>
 		<?php
-        $str = "The quick brown fox jumps over the lazy dog.";
-				
+        $str = "The quick brown fox jumps over the lazy dog.";	
         echo strToUpper(substr($str,24, 1)), substr($str,1,1), substr($str,-8,1), substr($str,14,1), substr($str,-11,1), substr($str,14,1), substr($str, 3,1), strToUpper(substr($str,-8,1)), substr($str,5,1), substr($str, -4,1), substr($str,-15,1), substr($str,-11,1), substr($str,-6,1), substr($str,-5,1), strToUpper(substr($str,23,1)), substr($str,-11,1), substr($str,-15,1), substr($str,-11,1), substr($str,-7,1);
     ?>
 		</h1>
